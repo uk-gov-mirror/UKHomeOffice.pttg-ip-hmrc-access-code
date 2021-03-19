7407164aac78ff848e8809eb07b609dc92cdc953
Anthony Nettleship <anthony.nettleship@bjss.com>
2021-03-19 11:44:02 +0000

    [EE-36241] Set lang to 11. Removed legacy Findbugs

--- src/test/java/uk/gov/digital/ho/pttg/application/SpringConfigurationTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/application/SpringConfigurationTest.java
@@ -1,7 +1,6 @@
 package uk.gov.digital.ho.pttg.application;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.time.Duration;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,7 +24,6 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
-@SuppressFBWarnings(value = "RV_RETURN_VALUE_IGNORED_INFERRED")
 public class SpringConfigurationTest {
 
     @Mock
